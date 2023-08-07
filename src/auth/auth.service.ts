74f93f5468f956a62b8e83e2f835a00469d67108
juleni <69912179+juleni@users.noreply.github.com>
2023-08-07 12:19:33 +0200

    Removed umused code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
