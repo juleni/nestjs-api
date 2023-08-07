6d72be27d9733269810af6d046fdf6da33d218e9
juleni <69912179+juleni@users.noreply.github.com>
2023-08-07 14:01:31 +0200

    Added Auth DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return { msg: 'signed in' };
+  signup(dto: AuthDto) {
+    return { msg: 'signed up' };
   }
 
-  signup() {
-    return { msg: 'signed up' };
+  signin() {
+    return { msg: 'signed in' };
   }
 }
